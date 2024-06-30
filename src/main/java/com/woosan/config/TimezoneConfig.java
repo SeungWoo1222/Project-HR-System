33a712b37a1fc33db158de6ae62ed779d8a8d0ed
san <sango7199@gmail.com>
2024-06-30 22:58:11 +0900

    타임존 설정

---
+++ src/main/java/com/woosan/config/TimezoneConfig.java
@@ -0,0 +1,14 @@
+package com.woosan.config;
+
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+import java.util.TimeZone;
+
+@Component
+public class TimezoneConfig {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+}
