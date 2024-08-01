3188cbb60998bbb8f95f2978e2f149f95ae485e0
Bemay <wlstmddn98@naver.com>
2024-08-01 16:57:00 +0900

    모달 내 js 기능 수정

--- src/main/resources/static/js/report.js
+++ src/main/resources/static/js/report.js
@@ -17,80 +17,80 @@ function toggleRejectionReason() {
 //     employeeNameField.disabled = !specificOption.checked;
 // }
 
-// 임원 선택 시, 부서 선택하고 임원을 선택함
-$(document).ready(function() {
-    // 초기에는 전체 선택 체크박스 숨기기
-    $('#selectAllContainer').hide();
+// 초기 설정 함수 호출
 
-    // 부서 선택 시 직원 목록을 불러오는 함수
-    $('#departmentId').on('change', function() {
-        console.log("실행됨");
-
-        var departmentId = $(this).val();
-        console.log("departmentId : " + departmentId);
-
-        $.ajax({
-            url: '/admin/request/employee', // API 엔드포인트를 설정
-            method: 'GET',
-            data: { departmentId: departmentId },
-            success: function(data) {
-                const select = $('#writerIdContainer');
-                select.empty(); // 기존 옵션 초기화
-                data.forEach(function(employee) {
-                    const checkboxContainer = $('<div></div>');
-                    const checkbox = $('<input type="checkbox" class="writerIdCheckbox" name="writerId" value="' + employee.employeeId + '">');
-                    const nameInput = $('<input type="hidden" class="writerNameInput" name="writerName" value="' + employee.name + '">');
-                    const label = $('<label>' + employee.name + '</label>');
-                    checkboxContainer.append(checkbox).append(label).append(nameInput);
-                    select.append(checkboxContainer);
-                });
-                $('#selectAllContainer').show(); // 부서 선택 후에 전체 선택 체크박스 보이기
-            },
-            error: function(error) {
-                console.error('Error:', error);h
-            }
-        });
-    });
-
-    // 모든 직원을 선택/해제하는 함수
-    $('#selectAllEmployeesCheckbox').on('change', function() {
-        const isChecked = $(this).prop('checked');
-        $('.writerIdCheckbox').prop('checked', isChecked);
-        addSelectedEmployees();
-    });
-
-    // 선택된 직원을 목록에 추가하는 함수
-    $('#writerIdContainer').on('change', '.writerIdCheckbox', function() {
-        addSelectedEmployees();
-    });
-
-    function addSelectedEmployees() {
-        const selectedContainer = $('#selectedEmployees');
-        selectedContainer.empty();
-        $('.writerIdCheckbox:checked').each(function() {
-            const id = $(this).val();
-            const name = $(this).siblings('.writerNameInput').val(); // name을 가져오기
-            const employeeDiv = $('<div class="selected-employee" data-id="' + id + '"></div>');
-            employeeDiv.append('<span>' + name + '</span>');
-            const removeButton = $('<button type="button">x</button>');
-            removeButton.on('click', function() {
-                $(this).parent().remove();
-                $('#writerIdContainer input[value="' + id + '"]').prop('checked', false);
-            });
-            employeeDiv.append(removeButton);
-            selectedContainer.append(employeeDiv);
-        });
-    }
-
+function setEventHandlers() {
     // 폼 제출 시 체크되지 않은 writerName 필드 제거
-    $('form').on('submit', function(event) {
-        $('.writerIdCheckbox').each(function() {
-            if (!$(this).is(':checked')) {
-                $(this).siblings('.writerNameInput').remove();
-            }
-        });
+    $('form').on('submit', removeUncheckedWriterNames);
+}
+
+function loadEmployeesByDepartment() {
+
+    var departmentId = $('#departmentId').val();
+
+    $.ajax({
+        url: '/admin/request/employee', // API 엔드포인트를 설정
+        method: 'GET',
+        data: { departmentId: departmentId },
+        success: function(data) {
+            populateEmployeeList(data);
+            $('#selectAllContainer').html('<label for="selectAllEmployeesCheckbox">부서 임원 전체 선택</label><input type="checkbox" id="selectAllEmployeesCheckbox">');
+            $('#selectAllContainer').show(); // 부서 선택 후에 전체 선택 체크박스 보이기
+
+            // 새로 생성된 체크박스에 이벤트 핸들러 설정
+            $('#selectAllEmployeesCheckbox').on('change', toggleSelectAllEmployees);
+        },
+        error: function(error) {
+            console.error('Error:', error);
+        }
     });
-});
+}
+
+function populateEmployeeList(data) {
+    const select = $('#writerIdContainer');
+    select.empty(); // 기존 옵션 초기화
+    data.forEach(function(employee) {
+        const checkboxContainer = $('<div></div>');
+        const checkbox = $('<input type="checkbox" class="writerIdCheckbox" name="writerId" value="' + employee.employeeId + '">');
+        const nameInput = $('<input type="hidden" class="writerNameInput" name="writerName" value="' + employee.name + '">');
+        const label = $('<label>' + employee.name + '</label>');
+        checkboxContainer.append(checkbox).append(label).append(nameInput);
+        select.append(checkboxContainer);
+    });
+}
+
+function toggleSelectAllEmployees() {
+    const isChecked = $('#selectAllEmployeesCheckbox').prop('checked');
+    $('.writerIdCheckbox').prop('checked', isChecked);
+    addSelectedEmployees();
+}
+
+function addSelectedEmployees() {
+    const selectedContainer = $('#selectedEmployees');
+    selectedContainer.empty();
+    $('.writerIdCheckbox:checked').each(function() {
+        const id = $(this).val();
+        const name = $(this).siblings('.writerNameInput').val(); // name을 가져오기
+        const employeeDiv = $('<div class="selected-employee" data-id="' + id + '"></div>');
+        employeeDiv.append('<span>' + name + '</span>');
+        const removeButton = $('<button type="button">x</button>');
+        removeButton.on('click', function() {
+            $(this).parent().remove();
+            $('#writerIdContainer input[value="' + id + '"]').prop('checked', false);
+        });
+        employeeDiv.append(removeButton);
+        selectedContainer.append(employeeDiv);
+    });
+}
+
+function removeUncheckedWriterNames(event) {
+    $('.writerIdCheckbox').each(function() {
+        if (!$(this).is(':checked')) {
+            $(this).siblings('.writerNameInput').remove();
+        }
+    });
+}
+
 
 // 통계 - 선택된 임원 목록 중 임원을 삭제하는 메소드
 function removeWriter(writerId) {
