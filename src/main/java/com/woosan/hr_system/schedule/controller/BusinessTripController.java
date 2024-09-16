b90c041fa1eb056dc1a0740ae5d138121f3cced2
San Jung <sango7199@gmail.com>
2024-09-16 21:29:52 +0900

    사이드바 아이콘 변경 및 일정 추가

--- src/main/java/com/woosan/hr_system/schedule/controller/BusinessTripController.java
+++ src/main/java/com/woosan/hr_system/schedule/controller/BusinessTripController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/businessTrip")
+@RequestMapping("/trip")
 public class BusinessTripController {
     @Autowired
     private BusinessTripService businessTripService;
